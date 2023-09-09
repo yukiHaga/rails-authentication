15f169631be4c6ee1912c92f4a750d6a599f5bc7
yukiHaga <haga@wcdi.co.jp>
2023-09-09 15:45:05 +0900

    Fix: name

--- backend-rails/app/controllers/users_controller.rb
+++
@@ -1,8 +0,0 @@
-class UsersController < ApplicationController
-  before_action :require_login, only: %i(show)
-
-  # application_controllerでログインしているかどうかをチェックしている
-  def show
-    @name = current_user.name
-  end
-end
--- backend-rails/app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 50 }
-  # emailが一意であるか
-  validates :email, presence: true, length: { maximum: 255 },
-                    uniqueness: true
-  validates :password, presence: true
-
-  has_secure_password
-end
